118de3a5c04c5c417fdb19e72dba5ee976b047ed
Ewerton Rafael <ton_rafael@hotmail.com>
2022-08-05 03:29:46 -0300

    e-commerce em Angular

---
+++ DesafioAngular/src/app/components/bookstore-app/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { BooksService } from './product-list.components.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  livros: any
+  bookService : BooksService
+  constructor(bookService: BooksService) {
+    this.bookService = bookService
+  }
+
+  ngOnInit(): void {
+
+    this.livros = this.bookService.getBook().subscribe((data => {
+
+      this.livros = data;
+    }))
+
+  }
+
+}
